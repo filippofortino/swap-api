2b36dc83774aad7406fa9528227276d065e15f75
Filippo Fortino <filippofortino@gmail.com>
2020-07-27 18:23:45 +0200

    Serve api from subdomain

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -72,7 +72,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        Route::prefix('api')
+        Route::domain(config('api_url'))
             ->middleware('api')
             ->namespace($this->namespace)
             ->group(base_path('routes/api.php'));
